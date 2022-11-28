f2788ecb101001f838c84979f8ce8eca9f818eb5
Hikanyan <hikanyan0131@gmail.com>
2022-11-29 07:06:25 +0900

    【Add】PlayerController

---
+++ Assets/Hikanyan_Assets/Script/PlayerController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]int  _playerSpeed;
+
+    
+    private void Start()
+    {
+        
+    }
+    private void Update()
+    {
+        
+    }
+}
