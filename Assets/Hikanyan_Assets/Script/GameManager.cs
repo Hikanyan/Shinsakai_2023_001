dc9c1a70c4e949567d9ae059287cc446bd56587d
Hikanyan <hikanyan0131@gmail.com>
2022-12-11 21:44:35 +0900

    【Update】GameManager

--- Assets/Hikanyan_Assets/Script/GameManager.cs
+++ Assets/Hikanyan_Assets/Script/GameManager.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class GameManager : SingletonBehaviour<GameManager>
 {
 
-
+    [SerializeField]
+    float waitTime = 2;
+    [SerializeField]
+    Text centerText;
+    [System.NonSerialized]
+    public bool gameOver = false;
     protected override void OnAwake()
     {
 
@@ -14,4 +19,31 @@ public class GameManager : SingletonBehaviour<GameManager>
     {
         
     }
+
+    public IEnumerator GameStart()
+    {
+        yield return new WaitForSeconds(waitTime);
+        centerText.enabled = true;
+        centerText.text = "3";
+        yield return new WaitForSeconds(1);
+        centerText.text = "2";
+        yield return new WaitForSeconds(1);
+        centerText.text = "1";
+        yield return new WaitForSeconds(1);
+        centerText.text = "GO!!";
+        yield return new WaitForSeconds(1);
+        centerText.text = "";
+        centerText.enabled = false;
+    }
+    public IEnumerator GameOver()
+    {
+        gameOver = true;
+        centerText.enabled = true;
+        centerText.text = "Game Over";
+        yield return new WaitForSeconds(waitTime);
+        centerText.text = "";
+        centerText.enabled = false;
+        gameOver = false;
+    }
+
 }
