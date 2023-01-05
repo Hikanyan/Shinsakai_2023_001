7aa9262bf06db7359c4c749d49a8a6152a389160
Hikanyan <hikanyan0131@gmail.com>
2023-01-06 00:41:23 +0900

    【Update】PlayerStatus

--- Assets/Hikanyan_Assets/Script/Player/PlayerStatus.cs
+++ Assets/Hikanyan_Assets/Script/Player/PlayerStatus.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.Rendering.HighDefinition.ScalableSettingLevelParameter;
 
 [CreateAssetMenu(fileName = "PlayerStatus", menuName = "CreatePlayerStatus")]
 public class PlayerStatus : ScriptableObject
@@ -21,23 +22,82 @@ public class PlayerStatus : ScriptableObject
     [SerializeField, Tooltip("会心ダメージ")]
     float _criticalDamage;
 
+    public int SetLevel(int lv)
+    {
+        _level = lv;
+        return _level;
+    }
+    public int GetLevel(int lv)
+    {
+        return _level;
+    }
+
     public int SetHp(int hp)
     {
         if (hp <= 0)
         {
             hp = 0;
         }
-        this._maxHp = hp;
-        return this._maxHp;
+        _maxHp = hp;
+        return _maxHp;
     }
 
     public int GetHp()
     {
-        return this._maxHp;
+        return _maxHp;
+    }
+    public int SetAttack(int atk)
+    {
+        _attack = atk;
+        return _attack;
+    }
+
+    public int GetAttack(int atk)
+    {
+        return _attack;
+    }
+    public int SetDefemse(int def)
+    {
+        _defense = def;
+        return _defense;
+    }
+    public int GetDefense(int def)
+    {
+        return _defense;
+    }
+    public int SetStamina(int stamina)
+    {
+        _maxStamina = stamina;
+        return _maxStamina;
+    }
+    public int GetStamina(int stamina)
+    {
+        return _maxStamina;
+    }
+    public float SetCriticalRate(float cr)
+    {
+        _criticalRate = cr;
+        return _criticalRate;
+    }
+    public float GetCriticalRate(float cr)
+    {
+        return _criticalDamage;
+    }
+    public float SetCriticalDamage(float cr)
+    {
+        _criticalDamage = cr;
+        return _criticalDamage;
     }
 
     public void Reset()
     {
-        this._maxHp = 10;
+        _level = 1;
+        _maxHp = 10;
+        _attack = 1;
+        _defense = 0;
+        _maxStamina = 10;
+
+        _criticalRate = 1.0f;
+        _criticalDamage = 1.0f;
     }
 }
